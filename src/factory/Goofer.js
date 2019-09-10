702f9d2152468b045d4d4456fe03a3c33f66e9d3
pierrealbaret <pierre.albaret@gmail.com>
2019-09-10 09:39:46 +0200

    MOD: ajout des déplacements disponible pour le Goofer

--- src/factory/Goofer.js
+++ src/factory/Goofer.js
@@ -2,6 +2,26 @@ export default class Goofer {
   constructor(rangeMin, rangeMax) {
     this.x = Math.floor(Math.random() * (rangeMax - rangeMin + 1)) + rangeMin;
     this.y = Math.floor(Math.random() * (rangeMax - rangeMin + 1)) + rangeMin;
+    this.direction = 'up';
+  }
+
+  movePosition() {
+    switch (this.direction) {
+      case 'up':
+        this.y = this.y -1;
+        break;
+      case 'down':
+        this.y = this.y +1;
+        break;
+      case 'right':
+        this.x = this.x + 1;
+        break;
+      case 'left':
+        this.x = this.x - 1;
+        break;
+      default:
+        break;
+    }
   }
 
   getPosition() {
